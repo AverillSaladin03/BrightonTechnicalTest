053d61eb94149ebd7dd547ad263c242a408083b4
AverillSaladin03 <averillsaladin2003@gmail.com>
2024-08-15 09:03:24 +0700

    Initial Commit

---
+++ brightontechnicaltest/brightontechnicaltestApp.swift
@@ -0,0 +1,17 @@
+//
+//  brightontechnicaltestApp.swift
+//  brightontechnicaltest
+//
+//  Created by Averill Saladin Atma Setiawan on 15/08/24.
+//
+
+import SwiftUI
+
+@main
+struct brightontechnicaltestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
